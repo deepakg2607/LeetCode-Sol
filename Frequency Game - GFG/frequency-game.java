f54db808cbedd03f1f7657bff2c0687aafcdb877
deepakg2607 <127415968+deepakg2607@users.noreply.github.com>
2023-05-31 00:08:20 +0530

    Added solution - LeetHub

---
+++ Frequency Game - GFG/frequency-game.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+// Position this line where user code will be pasted.
+
+// Driver class with main function
+class GFG {
+    // Main function
+    public static void main(String[] args)throws IOException {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+
+        // Iterating over testcases
+        while (t-- > 0) {
+            int n = Integer.parseInt(in.readLine());
+            int arr[] = new int[n];
+
+            String s[]=in.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(s[i]);
+
+            Solution obj = new Solution();
+            System.out.println(obj.LargButMinFreq(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+// Helper class to find largest number with minimum frequency
+class Solution {
+    // Function to find largest number with minimum frequency
+    public static int LargButMinFreq(int arr[], int n) {
+         HashMap<Integer, Integer> map=new HashMap<>();
+         for(int i=0;i<n;i++)
+         {
+             if(map.containsKey(arr[i]))
+             {
+                 map.put(arr[i], map.get(arr[i])+1);
+             }
+             else{
+                 map.put(arr[i], 1);
+             }
+             
+         }
+         ArrayList<Integer> a=new ArrayList<>();
+         ArrayList<Integer> a1=new ArrayList<>();
+         for(int i:map.keySet())
+         {
+             a.add(map.get(i));
+         }
+         Collections.sort(a);
+         int d=a.get(0);
+         for(int i:map.keySet())
+         {
+             if(map.get(i)==d)
+             {
+                 a1.add(i);
+             }
+         }
+         return Collections.max(a1);
+    }
+}
