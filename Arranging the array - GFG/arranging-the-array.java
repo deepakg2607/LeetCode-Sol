ba452c6930d0684ca5539a96a01943a9f6afc2a6
deepakg2607 <127415968+deepakg2607@users.noreply.github.com>
2023-06-10 19:44:20 +0530

    Added solution - LeetHub

---
+++ Arranging the array - GFG/arranging-the-array.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            obj.Rearrange(a, n);
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<n;i++)
+                output.append(a[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    public void Rearrange(int a[], int n)
+    {
+       ArrayList<Integer> a1=new ArrayList<>();
+       for(int i=0;i<n;i++)
+       {
+           if(a[i]<0)
+           {
+               a1.add(a[i]);
+           }
+       }
+       for(int i=0;i<n;i++)
+       {
+           if(a[i]>=0)
+           {
+               a1.add(a[i]);
+           }
+       }
+       for(int i=0;i<a1.size();i++)
+       {
+          a[i]=a1.get(i);
+       }
+    }
+}
