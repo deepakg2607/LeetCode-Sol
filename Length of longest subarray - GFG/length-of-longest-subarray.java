f11ff825044be9aa009fa334b027c1cb0d567168
deepakg2607 <127415968+deepakg2607@users.noreply.github.com>
2023-06-13 20:14:47 +0530

    Added solution - LeetHub

---
+++ Length of longest subarray - GFG/length-of-longest-subarray.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            System.out.println(obj.longestSubarry(a, n));
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute {
+    
+    public long longestSubarry(long A[], int N)
+    {
+        int count=0;
+        int max=0;
+        for(int i=0;i<N;i++)
+        {
+            if(A[i]>=0)
+            {
+                count++;
+            }
+            else{
+                max=Math.max(max, count);
+                count=0;
+            }
+            
+        }
+        max=Math.max(max, count);
+        return max;
+    }
+}
