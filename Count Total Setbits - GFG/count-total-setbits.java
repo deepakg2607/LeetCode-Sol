a6a834f69c495150c46cfd4281a5959af67e2de4
Deepak Kumar <127415968+deepakg2607@users.noreply.github.com>
2023-06-30 02:17:08 +0530

    Added solution - LeetHub

--- Count Total Setbits - GFG/count-total-setbits.java
+++ Count Total Setbits - GFG/count-total-setbits.java
@@ -1,38 +1,36 @@
 //{ Driver Code Starts
+//Initial Template for Java
+
 import java.io.*;
 import java.util.*;
+import java.lang.*;
 
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t;
-        t = Integer.parseInt(br.readLine());
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
         while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
             
-            long N;
-            N = Long.parseLong(br.readLine().trim());
-            
-            Solution obj = new Solution();
-            long res = obj.countBits(N);
-            
-            System.out.println(res);
-            
+            Solution ob = new Solution();
+            System.out.println(ob.countBits(N));
         }
     }
 }
-
 // } Driver Code Ends
 
 
-class Solution {
-    public static long countBits(long N) {
+//User function Template for Java
+
+class Solution{
+    static int countBits(int N){
         // code here
-        long sum=0;
-        for(long i=1;i<=N;i++)
+        int sum=0;
+        for(int i=1;i<=N;i++)
         {
-            sum=sum+Long.bitCount(i);
+            sum=sum+Integer.bitCount(i);
         }
         return sum;
     }
 }
-        
