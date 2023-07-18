66033410aa5c552f0990fe7cffefd33c3d79d159
Deepak Kumar <deepakgupta.261207@gmail.com>
2023-07-18 22:39:10 +0530

    Added solution - LeetHub

--- Max Sum without Adjacents - GFG/max-sum-without-adjacents.java
+++ Max Sum without Adjacents - GFG/max-sum-without-adjacents.java
@@ -45,7 +45,7 @@ class Solution {
             return dp[n];
         }
         int include=solve(arr , n-2 , dp)+ arr[n];
-        int exclude=solve(arr , n-1 , dp);
+        int exclude=solve(arr , n-1 , dp)+0;
         dp[n]= Math.max(include , exclude);
         return dp[n];
     }
