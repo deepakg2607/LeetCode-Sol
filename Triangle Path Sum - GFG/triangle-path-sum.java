f8e1bd230dd3911daabb5e43faf7ea2acadbbf31
Deepak Kumar <deepakgupta.261207@gmail.com>
2023-10-01 19:59:15 +0530

    Added solution - LeetHub

---
+++ Triangle Path Sum - GFG/triangle-path-sum.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            ArrayList<ArrayList<Integer> > triangle= new ArrayList<ArrayList<Integer> >(n);
+            for (int i = 0; i < n; i++) {
+                ArrayList<Integer>temp=new ArrayList<Integer>();
+                for(int j=0;j<=i;j++){
+                    int x=sc.nextInt();
+                    temp.add(x);
+                }
+                triangle.add(temp);
+            }
+            Solution obj = new Solution();
+            int ans = obj.minimizeSum(n, triangle);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public static int minimizeSum(int n, ArrayList<ArrayList<Integer> >triangle) {
+        // Code here
+        //  int n = triangle.size();
+        int[][] dp = new int[n][n];
+        
+        for (int i = 0; i < n; i++) {
+            dp[n-1][i] = triangle.get(n-1).get(i);
+        }
+        
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i; j >= 0; j--) {
+                dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        
+        return dp[0][0];
+        
+    }
+}
